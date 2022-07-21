e8cbdb25df68f7eeea26e00d78861a18997a3f5e
Sebastian <sebastian.besel@nethesis.it>
2022-07-21 21:39:31 +0200

    Fix linting step

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
 
-export const Button = (props) => {
-    return (
-        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4
+export const Button = props => {
+  return (
+    <button
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4
                           focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 
-                          dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-40">
-                            Login
-        </button>
-    );
-}
+                          dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-40"
+    >
+      Login
+    </button>
+  );
+};
--- src/components/Input/TextInput.jsx
+++ src/components/Input/TextInput.jsx
@@ -1,12 +1,14 @@
+import React from 'react';
 
-
-import React from "react";
-
-export const TextInput = (props) => {
-    return (
-      <input type="text" id="small-input" class="block p-2 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs
+export const TextInput = props => {
+  return (
+    <input
+      type="text"
+      id="small-input"
+      class="block p-2 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs
             focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 
-            dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center mr-2 mb-2" 
-              placeholder="Username"/>
-    );
-}
+            dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center mr-2 mb-2"
+      placeholder="Username"
+    />
+  );
+};
