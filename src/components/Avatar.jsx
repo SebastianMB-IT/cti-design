c60e62fbe30b1ef06cbee4819bdbf7c43259f531
Tony Colapietro <tonyco97.tc@gmail.com>
2022-07-26 15:38:25 +0200

    Removed static image

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Profile from '/src/Images/profilePicture.jpg';
+// import Profile from '/src/Images/profilePicture.jpg';
 // import PropTypes from 'prop-types';
 
 export const Avatar = props => {
@@ -7,7 +7,7 @@ export const Avatar = props => {
     <div>
       <img
         className="p-1 w-10 h-10 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
-        src={Profile}
+        src='https://picsum.photos/200/300/?blur'
         alt="Profile picture"
       />
     </div>
