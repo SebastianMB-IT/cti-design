d8bcbac839873c50308a10bb70ed5bdc37a7d9e7
Antonio <36625268+tonyco97@users.noreply.github.com>
2022-08-02 16:09:24 +0200

    Fixed Alert component (#6)

--- stories/Atoms/Alert.stories.jsx
+++ stories/Atoms/Alert.stories.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Alert } from './../../src';
 import { HiLightningBolt } from "react-icons/hi";
+import {HiCheck} from "react-icons/hi";
+import {VscInfo} from "react-icons/vsc";
+
 
 const meta = {
   title: 'Components/Atoms/Alert',
   component: Alert,
   argTypes: {
     onClick: { action: 'clicked' },
-    backgroundColor: { control: 'color' },
+    color: { control: { type: 'color', presetColors: ['red', 'green', 'yellow', 'blue']} },
+    backgroundColor: { control: { type: 'color', presetColors: ['red', 'green', 'yellow', 'blue']} },
     enabled: {
     },
   },
@@ -20,8 +24,36 @@ export default meta;
 
 const Template = (args) => <Alert {...args} />;
 
-export const Content = Template.bind({});
-Content.args = {
-  alertType: "Write the type of the alert here!",
+export const Error = Template.bind({});
+Error.args = {
+  color: "red",
+  backgroundColor: "red",
   icon: <HiLightningBolt />,
+};
+
+export const warning = Template.bind({});
+warning.args = {
+  alertTitle: "Warning",
+  alertType: "Something maybe wrong!",
+  icon: <VscInfo />,
+  color: '#ffc107',
+  backgroundColor: '#ffc107',
+};
+
+export const success = Template.bind({});
+success.args = {
+  alertTitle: "Success",
+  alertType: "Something went Ok!",
+  color: '#28a745',
+  backgroundColor: '#28a745',
+  icon: <HiCheck />,
+};
+
+export const info = Template.bind({});
+info.args = {
+  alertTitle: "Information",
+  alertType: "Something to know!",
+  icon: <VscInfo />,
+  color: '#17a2b8',
+  backgroundColor: '#17a2b8',
 };
