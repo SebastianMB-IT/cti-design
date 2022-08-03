90ba5c2a2f555ae9c8fea51b5c3d371d81141b8d
Antonio <36625268+tonyco97@users.noreply.github.com>
2022-08-03 11:13:18 +0200

    Fixed the Dropdown component (#8)

--- src/components/Dropdown/Dropdown.stories.jsx
+++ src/components/Dropdown/Dropdown.stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Dropdown } from '../..';
+import { RiArrowDownSLine } from 'react-icons/ri';
 import { HiDotsVertical } from 'react-icons/hi';
 
 const meta = {
@@ -20,13 +21,25 @@ const meta = {
 
 export default meta;
 
-const Template = args => <Dropdown {...args} />;
+const Template = (args) => <Dropdown {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Text = Template.bind({});
+Text.args = {
+  dropdownTitle: 'Choose options',
+  option1: 'Option 1',
+  option2: 'Option 2',
+  option3: 'Option 3',
+  option4: 'Option 4',
+  icon: <RiArrowDownSLine />,
+  isText: true,
+};
+
+export const Icon = Template.bind({});
+Icon.args = {
   option1: 'Option 1',
   option2: 'Option 2',
   option3: 'Option 3',
   option4: 'Option 4',
   icon: <HiDotsVertical />,
+  isIcon: true,
 };
