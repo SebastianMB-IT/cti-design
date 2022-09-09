875c212d81545bb978c0e19e895e32dbb73db148
Sebastian <sebastian.besel@nethesis.it>
2022-09-09 11:21:20 +0200

    Enhance TextInput z-index

--- src/components/Input/TextInput.stories.tsx
+++ src/components/Input/TextInput.stories.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Meta, Story } from '@storybook/react'
-import { TextInput, TextInputProps } from '../..'
-import { HiMail } from 'react-icons/hi'
-import { MdError } from 'react-icons/md'
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { TextInput, TextInputProps } from '../..';
+import { HiMail } from 'react-icons/hi';
+import { MdError } from 'react-icons/md';
 
 const meta = {
   title: 'Components/Input/TextInput',
@@ -19,68 +19,68 @@ const meta = {
   parameters: {
     controls: { expanded: true },
   },
-}
+};
 
-export default meta as Meta
+export default meta as Meta;
 
-const Template: Story<TextInputProps> = (args) => <TextInput {...args} />
+const Template: Story<TextInputProps> = (args) => <TextInput {...args} />;
 
-export const Base = Template.bind({})
+export const Base = Template.bind({});
 Base.args = {
   placeholder: 'Input placeholder',
-}
+};
 
-export const Large = Template.bind({})
+export const Large = Template.bind({});
 Large.args = {
   placeholder: 'Input placeholder',
   name: 'test',
   size: 'large',
-}
+};
 
-export const WithLabel = Template.bind({})
+export const WithLabel = Template.bind({});
 WithLabel.args = {
   label: 'Input label',
   placeholder: 'Input placeholder',
   name: 'test',
-}
+};
 
-export const WithLeadingIcon = Template.bind({})
+export const WithLeadingIcon = Template.bind({});
 WithLeadingIcon.args = {
   ...WithLabel.args,
   icon: HiMail,
-}
+};
 
-export const WithTrailingIcon = Template.bind({})
+export const WithTrailingIcon = Template.bind({});
 WithTrailingIcon.args = {
   ...WithLabel.args,
   icon: HiMail,
   iconRight: true,
-}
+};
 
-export const WithError = Template.bind({})
+export const WithError = Template.bind({});
 WithError.args = {
   ...WithLabel.args,
   error: true,
-}
+};
 
-export const ErrorWithHelper = Template.bind({})
+export const ErrorWithHelper = Template.bind({});
 ErrorWithHelper.args = {
   ...WithLabel.args,
   helper: 'This is a helper text',
   error: true,
-}
+};
 
-export const ErrorHelperWithIcon = Template.bind({})
+export const ErrorHelperWithIcon = Template.bind({});
 ErrorHelperWithIcon.args = {
   ...WithLabel.args,
   icon: MdError,
   iconRight: true,
   helper: 'This is a helper text',
   error: true,
-}
+};
 
-export const Squared = Template.bind({})
+export const Squared = Template.bind({});
 Squared.args = {
   ...WithLabel.args,
   squared: 'right',
-}
+};
