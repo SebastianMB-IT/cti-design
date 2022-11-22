4db7f2605038ceb5f2e8e448232017be7bfaeaa7
Sebastian <sebastian.besel@nethesis.it>
2022-11-22 15:03:00 +0100

    Update theme and package.json

--- src/theme/Theme.ts
+++ src/theme/Theme.ts
@@ -230,7 +230,7 @@ const theme = {
     image: 'w-full h-full',
     bordered: 'border-2 border-white',
     initials: {
-      base: 'text-white w-full h-full flex justify-center items-center text-xl',
+      base: 'text-white w-full h-full flex justify-center items-center',
       background: 'bg-gray-500',
     },
     placeholder: {
